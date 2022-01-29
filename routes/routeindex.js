aacd20b2d6988b46aeaa483b17210c1464117965
Victor CO <curiel.vf@gmail.com>
2022-01-29 15:12:14 -0600

    Se da formato a Index y Creador.

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express();
 const Creador = require('../model/creador');
-/*
-var juegos = [
-    { nombre: "Journey", creador: "Thatgamecompany", descripcion: "Journey es uno de los juegos más bonitos que existen. Con ausencia total de diálogos, el juego te lleva en un viaje de descubrimiento mientras te diriges hacia la cima de una montaña. Es posible avanzar en solitario o encontrarte con otros jugadores que están teniendo la misma experiencia que tú. En pocas palabras, Journey fascina por su simplicidad y riqueza visual.", imagen: "https://cdn.ligadegamers.com/imagenes/journey-mejores-juegos-indie.jpg" },
-    { nombre: "Stardew Valley", creador: "Chucklefish Games", descripcion: "Stardew Valley es un juego apasionante para hacer de granjero a solas o en compañía. Hay muchos tipos de actividades a llevar a cabo, misiones que cumplir y posibilidades infinitas para hacer un tipo de granja u otra. Es posible interactuar con bastantes NPCs y formar una familia, o lanzarte al peligro en búsqueda de tesoros. ¡Stardew Valley puede ser tan largo como quieras!", imagen: "https://cdn.ligadegamers.com/imagenes/stardew-valley-mejores-juegos-indie.jpg" },
-    { nombre: "Undertale", creador: "Toby Fox", descripcion: "Undertale es un RPG enfocado en las decisiones. Puedes matar, intentar dialogar o hasta reclutar enemigos como buenos aliados. Tu misión es salir del mundo subterráneo y regresar a la superficie. ¿Y qué te lo impide? Monstruos y mazmorras llenas de rompecabezas. ¿Serás un personaje de paz o arrasarás todo a tu paso? ¡Tú eliges!", imagen: "https://cdn.ligadegamers.com/imagenes/undertale-mejores-juegos-indie.jpg" },
-    { nombre: "Disco Elysium", creador: "ZA UM", descripcion: "El planteamiento de Disco Elysium es sublime. Se trata de un RPG distinto a lo que estamos acostumbrado, muy enfocado en la libertad de acción, decisión y construcción del personaje. Asumirás el control de un detective, quien debe resolver un crimen. Sin embargo, la borrachera hace que sufra de amnesia, ¡así que necesita tu ayuda! Interactúa con los personajes como quieras, conecta pistas y acusa a quienes creas que son culpables. ¡Reflexiona bien!", imagen: "https://cdn.ligadegamers.com/imagenes/disco-elysium-mejores-juegos-indie.jpg" },
-    { nombre: "Ori and the Blind Forest", creador: "Moon Studios", descripcion: "Ori and the Blind Forest posee una historia emotiva que viene acompañada de un apartado audiovisual maravilloso. Tiene una dificultad bien balanceada, en un juego que te reta a resolver rompecabezas, coleccionar ítems y salvar el mundo de la oscuridad. Si te apasionó este juego, ¡también está Ori and the Will of the Wisps!", imagen: "https://cdn.ligadegamers.com/imagenes/ori-and-the-blind-forest-mejores-juegos-indie.jpg" },
-]
-*/
+
 router.get("/", async (req, res) => {
     let creadores = await Creador.find()
-    
+
     juegos = []
     for (let i = 0; i < creadores.length; i++) {
         for (let j = 0; j < creadores[i].juegos.length; j++) {
@@ -21,5 +13,36 @@ router.get("/", async (req, res) => {
     }
     res.render("pages/index", { juegos })
 })
+
+//Ejemplo para añadir datos a base ----------------------------------
+router.get("/addDB", async (req, res) => {
+    /*
+    nombre: "", descripcion: "", imagen: "", sitio: "", correo: "", telefono: "", calificacion: 4.7,
+            juegos: [
+                { nombre: "", creador: "", descripcion: "", imagen: "" },
+            ]
+            */
+
+    let cre = {
+        nombre: "Thatgamecompany", descripcion: "Estudio independiente de desarrollo de videojuegos estadounidense, fundado por los estudiantes de la Universidad del Sur de California Kellee Santiago y Jenova Chen. Este estudio ha trabajado como desarrollador para Sony Computer Entertainment, en un contrato consistente en la creación de tres juegos descargables para la PlayStation Network.", imagen: "https://upload.wikimedia.org/wikipedia/en/3/3b/ThatGameCompany_Logo.png", sitio: "https://thatgamecompany.com/", correo: "info@thatgamecompany.com", telefono: "(310)453-4906", calificacion: 4.7,
+        juegos: [
+            { nombre: "Journey", creador: "Thatgamecompany", descripcion: "Journey es uno de los juegos más bonitos que existen. Con ausencia total de diálogos, el juego te lleva en un viaje de descubrimiento mientras te diriges hacia la cima de una montaña. Es posible avanzar en solitario o encontrarte con otros jugadores que están teniendo la misma experiencia que tú. En pocas palabras, Journey fascina por su simplicidad y riqueza visual.", imagen: "https://cdn.ligadegamers.com/imagenes/journey-mejores-juegos-indie.jpg" },
+            { nombre: "Sky: Niños de la Luz", creador: "Thatgamecompany", descripcion: "Sky: Niños de la Luz es un videojuego indie social de mundo abierto desarrollado y lanzado por Thatgamecompany. Su primer lanzamiento fue en iOS el 18 de Julio de 2019.", imagen: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_SkyChildrenOfTheLight_ES_image1600w.jpg" },
+        ]
+    } 
+    let cre2 = {
+        nombre: "ACE Team", descripcion: "ACE Team es una compañía independiente de videojuegos establecida en Santiago, Chile. Su nombre proviene de las iniciales de los 3 miembros fundadores: Andres Bordeu, Carlos Bordeu y Edmundo Bordeu. Ha licenciado con el motor Source con el que realizó su juego debut Zeno Clash.", imagen: "https://sm.ign.com/ign_latam/screenshot/default/ace-team-logo-black-600x366_dk86.jpg", sitio: "https://www.aceteam.cl/index.html", correo: "contact@aceteam.cl", telefono: "(650)723-4906", calificacion: 3.5,
+        juegos: [
+            { nombre: "The Eternal Cylinder", creador: "ACE Team", descripcion: "The Eternal Cylinder es un videojuego de exploración de mundo abierto de supervivencia de aventuras desarrollado por ACE Team y publicado por Good Shepherd Entertainment.", imagen: "https://media.vandal.net/m/76502/the-eternal-cylinder-20198161222137_1.jpg" },
+        ]
+    }
+
+    
+    let task = new Creador(cre)
+    let task2 = new Creador(cre2)
+    await task.save()
+    await task2.save()
+})
+//-------------------------------------------------------------------
 
 module.exports = router;
