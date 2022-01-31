8d383b9081efc3a60603c8580ef0eadd309b8475
Victor CO <curiel.vf@gmail.com>
2022-01-31 17:14:31 -0600

    Fix Perfil.

--- routes/routeperfil.js
+++ routes/routeperfil.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express();
+const Creador = require('../model/creador');
 
 router.get("/", async (req, res) => {
-    res.render("pages/perfil")
+    let creadores = await Creador.find()
+    
+    for (let i = 0; i < creadores.length; i++) {
+
+        if (creadores[i].nombre === "Thatgamecompany") {
+            res.render("pages/perfil", creadores[i])
+        }
+    }
 })
 
 module.exports = router;
