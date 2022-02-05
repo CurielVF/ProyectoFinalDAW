b0cd6c862226f2ecc4d034d43ff40ec23d703946
Rodolfo <rodolfiux1@hotmail.com>
2022-02-05 14:24:49 -0600

    Coemntarios

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -48,7 +48,8 @@ router.get("/addDB", async (req, res) => {
         descripcion: "Journey es uno de los juegos más bonitos que existen. Con ausencia total de diálogos, el juego te lleva en un viaje de descubrimiento mientras te diriges hacia la cima de una montaña. Es posible avanzar en solitario o encontrarte con otros jugadores que están teniendo la misma experiencia que tú. En pocas palabras, Journey fascina por su simplicidad y riqueza visual.",
         imagen: "https://cdn.ligadegamers.com/imagenes/journey-mejores-juegos-indie.jpg",
         clasificacion: "+18",
-        categoria: "Shootter"
+        categoria: "Shootter",
+        comentarios:""
     }
 
     let jue2 = {
@@ -57,7 +58,8 @@ router.get("/addDB", async (req, res) => {
         descripcion: "Sky: Niños de la Luz es un videojuego indie social de mundo abierto desarrollado y lanzado por Thatgamecompany. Su primer lanzamiento fue en iOS el 18 de Julio de 2019.",
         imagen: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_SkyChildrenOfTheLight_ES_image1600w.jpg",
         clasificacion: "+18",
-        categoria: "Shootter"
+        categoria: "Shootter",
+        comentarios:"El mejor juego"
     }
 
     let task3 = new Juego(jue)
